26f8cd57e71ac796c25fe197eca9080c6710499b
lpoliveiraneto <leonidespires@gmail.com>
2023-12-26 16:16:22 -0300

    aula 3

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import IProjeto from "@/interfaces/IProjetos";
 import { Store, createStore, useStore as vuexUseStore} from "vuex";
 import { InjectionKey } from "vue";
+import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipoMutacoes";
 
 interface Estado{
     projetos: IProjeto[]
@@ -14,7 +15,7 @@ export const store = createStore<Estado>({
         
     }, 
     mutations:{
-        ['ADICIONA_PROJETO'](state, nomeDoProjeto: string){
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string){
             console.log('salvando o projeto')
             const projeto = {
                 id: new Date().toISOString(),
@@ -22,10 +23,13 @@ export const store = createStore<Estado>({
             } as IProjeto
             state.projetos.push(projeto)
         },
-        ['ALTERA_PROJETO'](state, projeto: IProjeto){
+        [ALTERA_PROJETO](state, projeto: IProjeto){
             console.log('Editando o projeto')
             const index = state.projetos.findIndex(proj => proj.id == projeto.id)
             state.projetos[index] = projeto
+        },
+        [EXCLUIR_PROJETO](state, id:string){
+            state.projetos = state.projetos.filter(proj => proj.id != id)
         }
     }
 })
--- src/views/projetos/Formulario.vue
+++ src/views/projetos/Formulario.vue
@@ -19,6 +19,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useStore } from "@/store";
+import { ALTERA_PROJETO, ADICIONA_PROJETO } from "@/store/tipoMutacoes"
 //import ProjetosVue from "../Projetos.vue";
 
 export default defineComponent({
@@ -42,12 +43,12 @@ export default defineComponent({
   methods: {
     salvar() {  
       if (this.id) {
-        this.store.commit('ALTERA_PROJETO', {
+        this.store.commit(ALTERA_PROJETO, {
           id: this.id,
           nome: this.nomeDoProjeto
         })
       } else {
-        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+        this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
       }
       // if (this.id == 'novo') {
       //   console.log(this.id)
