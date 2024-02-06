e02e1dd5f6f67aef9967a8c875aa84cb0e6d11fb
lpoliveiraneto <leonidespires@gmail.com>
2024-02-06 11:52:09 -0300

    aula 2 - parte 1 do curso  vue3: api e vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import IProjeto from "@/interfaces/IProjetos";
 import { Store, createStore, useStore as vuexUseStore} from "vuex";
 import { InjectionKey } from "vue";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipoMutacoes";
+import { ADICIONAR_TAREFAS, ADICIONA_PROJETO, ALTERA_PROJETO, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipoMutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETOS, CADASTRAR_PROJETOS, OBTER_PROJETOS, REMOVER_PROJETOS } from "./tipo-acoes";
+import { ALTERAR_PROJETOS, CADASTRAR_PROJETOS, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETOS } from "./tipo-acoes";
 import http from "@/http"
+import ITarefa from "@/interfaces/ITarefas";
 
 interface Estado{
     projetos: IProjeto[],
+    tarefas: ITarefa[],
     notificacoes: INotificacao[]
 }
 
@@ -16,6 +18,7 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
   state: {
     projetos: [],
+    tarefas: [],
     notificacoes: [],
   },
   mutations: {
@@ -36,36 +39,54 @@ export const store = createStore<Estado>({
       state.projetos = state.projetos.filter((proj) => proj.id != id);
     },
     [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
-      state.projetos = projetos
+      state.projetos = projetos;
     },
-    [NOTIFICAR] (state, novaNotificacao: INotificacao){
-        novaNotificacao.id = new Date().getTime()
-        state.notificacoes.push(novaNotificacao)
+    [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
+      state.tarefas = tarefas;
+    },
+    [NOTIFICAR](state, novaNotificacao: INotificacao) {
+      novaNotificacao.id = new Date().getTime();
+      state.notificacoes.push(novaNotificacao);
 
-
-        setTimeout(() => {
-            state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
-        }, 3000)
+      setTimeout(() => {
+        state.notificacoes = state.notificacoes.filter(
+          (notificacao) => notificacao.id != novaNotificacao.id
+        );
+      }, 3000);
+    },
+    [ADICIONAR_TAREFAS](state, tarefa: ITarefa) {
+      state.tarefas.push(tarefa)
     }
   },
-  actions:{
-    [OBTER_PROJETOS] ({ commit }){
-        http.get('projetos')
-          .then( resposta => commit(DEFINIR_PROJETOS, resposta.data))
+  actions: {
+    [OBTER_PROJETOS]({ commit }) {
+      http
+        .get("projetos")
+        .then((resposta) => commit(DEFINIR_PROJETOS, resposta.data));
     },
-    [CADASTRAR_PROJETOS] (contexto, nomeDoProjeto: string){
-       return http.post('/projetos', {
-          nome: nomeDoProjeto
-        })
+    [CADASTRAR_PROJETOS](contexto, nomeDoProjeto: string) {
+      return http.post("/projetos", {
+        nome: nomeDoProjeto,
+      });
     },
-    [ALTERAR_PROJETOS] (contexto, projeto: IProjeto){
-      return http.put(`/projetos/${projeto.id}`, projeto)
-   },
-   [REMOVER_PROJETOS] (contexto, id: string){
-    return http.delete(`/projetos/${id}`)
-      .then(() => this.commit(EXCLUIR_PROJETO, id))
- }
-  }
+    [ALTERAR_PROJETOS](contexto, projeto: IProjeto) {
+      return http.put(`/projetos/${projeto.id}`, projeto);
+    },
+    [REMOVER_PROJETOS](contexto, id: string) {
+      return http
+        .delete(`/projetos/${id}`)
+        .then(() => this.commit(EXCLUIR_PROJETO, id));
+    },
+    [OBTER_TAREFAS]({ commit }) {
+      http
+        .get("tarefas")
+        .then((resposta) => commit(DEFINIR_TAREFAS, resposta.data));
+    },
+    [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
+      return http.post("/tarefas", tarefa)
+        .then(resposta => commit(ADICIONAR_TAREFAS, resposta.data));
+    },
+  },
 });
 
 export function useStore(): Store<Estado>{
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -3,15 +3,33 @@
   <div class="lista">
     <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
     <Box v-if="listaEstaVazia"> VAI TRABALHAR CARALHO! </Box>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Modal title</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <!-- Content ... -->
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save changes</button>
+          <button class="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import ITarefa from "../interfaces/ITarefas";
 import Box from "../components/Box.vue";
+import { OBTER_TAREFAS, CADASTRAR_TAREFA, OBTER_PROJETOS } from "@/store/tipo-acoes";
+import { useStore } from '@/store';
 
 export default defineComponent({
   name: "App",
@@ -21,19 +39,23 @@ export default defineComponent({
     Tarefa,
     Box,
   },
-  data() {
-    return {
-      tarefas: [] as ITarefa[]
-    };
-  },
   computed: {
     listaEstaVazia(): boolean {
-      return this.tarefas.length === 0;
+      return this.tarefas.length == 0;
     },
+  },
+  setup() {
+    const store = useStore()
+    store.dispatch(OBTER_TAREFAS)
+    store.dispatch(OBTER_PROJETOS)
+    return {
+      tarefas: computed(() => store.state.tarefas),
+      store
+    }
   },
   methods: {
     salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
+      this.store.dispatch(CADASTRAR_TAREFA, tarefa)
     },
   },
 });
